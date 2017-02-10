2777e6382c49f0f70a5104368a42748c6cab9fb2
Jean Liu <hjliu415@gmail.com>
2017-02-09 16:45:14 -0800

    did min heap

--- practice_problems.py
+++ practice_problems.py
@@ -150,24 +150,159 @@
 
 ########################################################################
 
-def longest_distance(astr):
-    # takes in a string and returns the longest distance between a repeating char
-    char_dist = {}  # {char: [index at pos1, index at pos2]}
+# def longest_distance(astr):
+#     # takes in a string and returns the longest distance between a repeating char
+#     char_dist = {}  # {char: [index at pos1, index at pos2]}
 
-    for i in range(len(astr)):
-        char = astr[i]
-        char_dist[char] = char_dist.get(char, [i, i])
-        char_dist[char][1] = i
+#     for i in range(len(astr)):
+#         char = astr[i]
+#         char_dist[char] = char_dist.get(char, [i, i])
+#         char_dist[char][1] = i
 
-    longest_distance = 0
-    character = None
+#     longest_distance = 0
+#     character = None
 
-    for char in char_dist:
-        diff = char_dist[char][1] - char_dist[char][0]
-        if diff > longest_distance:
-            longest_distance = diff
-            character = char
+#     for char in char_dist:
+#         diff = char_dist[char][1] - char_dist[char][0]
+#         if diff > longest_distance:
+#             longest_distance = diff
+#             character = char
 
-    return character, longest_distance
+#     return character, longest_distance
 
-print longest_distance("sabybada") #a, 6
+# print longest_distance("sabybada") #a, 6
+
+
+##########################################################################
+# Min Heaps (same things apply to Max Heaps, just opposite :) )
+# practical applications: UBER/Lyft with closed car at the top, games: enemies closest
+
+# Inserting a node into a heap: O(logn)
+# when inserting a node into a min heap, it inserts breath first from the left most leaf. It bubbles up.
+#      if it is smaller than it's parent: swap
+
+# Deleting from a min heap:
+# Swap the peak with the last leaf (breath first). Trim and return it.
+# Look at the new peak. If it's larger than it's children, swap with it's smallest one. 
+# Keep bubbling down
+
+# A min heap/ binary tree can be represented by an array made with breath first search
+# Can keep track of parents and children with indexes: Pi and Ci
+# Ex. [4,7,5,9,10,6,8]
+#    Pi: 0  Ci: 1,2
+#    Pi: 1  Ci: 3,4
+#    Pi: 2  Ci: 5,6
+#   Relationship:  2n+1 (odd), 2n+2 (even)
+
+# Doing insertion using an array:
+#  - Push on the new element 1 onto the array. Ex. [4,7,5,9,10,6,8,1]
+#  - The parent of 1 is 9 (index 7 has parent at index 3). 1 < 9 = swap  Ex. [4,7,5,1,10,6,8,9]
+#  - The parent of 1 is now 7 (index 3 has parent at index 1). 1 < 7 = swap  Ex. [4,1,5,7,10,6,8,9]
+#  - The parent of 1 is now 4 (index 1 has parent at index 0). 1 < 4 = swap  Ex. [1,4,5,7,10,6,8,9]
+
+
+# Doing deletion using an array:  Ex. [1,4,5,7,10,6,8,9]
+#  - Swap the first with last   Ex. [9,4,5,7,10,6,8,1]
+#  - Take off the last one. Then swap the first with it's smaller child like above
+
+
+class MinHeap():
+    def __init__(self, alist=None):
+        self.alist = alist
+
+    def swap(self, i1, i2):
+        # given 2 indexes, it swaps the elements in the two indexes
+        self.alist[i1], self.alist[i2] = self.alist[i2], self.alist[i1]
+
+    def peak(self):
+        # return the topmost node
+        return self.alist[0]
+
+    def size(self):
+        # returns the length of the storage array
+        return len(self.alist)
+
+    def insert(self, value):
+        self.alist.append(value)
+        self.bubble_up(self.size - 1)
+
+    def bubble_up(self, ci):
+        # given a child index ci, find parent, swap if needed
+        # pi = parent index
+        if ci % 2 == 0:
+            pi = (ci - 2)/2
+        else:
+            pi = (ci - 1)/2
+
+        while ci > 0 and self.alist[ci] < self.alist[ci]:
+            self.swap(ci, pi)
+            ci = pi
+            if ci % 2 == 0:
+                pi = (ci - 2)/2
+            else:
+                pi = (ci - 1)/2
+
+    def remove_peak(self):
+        self.swap(0, self.size() - 1)
+        minElement = self.alist.pop()
+        self.bubble_down(0)
+        return minElement
+
+    def bubble_down(self, pi):
+        ci1 = (2 * pi) + 1
+        ci2 = (2 * pi) + 2
+
+        # make sure the children is there.
+        # Indexes have to be within the len
+        if ci1 >= self.size():
+            return None
+        elif ci2 >= self.size():
+            mci = ci1  # master child index
+        elif self.alist[ci1] < self.alist[ci2]:  # find the smaller child
+            mci = ci1
+        else:
+            mci = ci2
+        print self.alist[pi]
+        print self.alist[mci]
+        while pi < (self.size()-1) and self.alist[pi] > self.alist[mci]:
+            print pi, mci
+            self.swap(pi, mci)
+            pi = mci
+
+            ci1 = (2 * pi) + 1
+            ci2 = (2 * pi) + 2
+
+            if ci1 >= self.size():
+                return None
+            elif ci2 >= self.size():
+                mci = ci1
+            elif self.alist[ci1] < self.alist[ci2]:
+                mci = ci1
+            else:
+                mci = ci2
+            print "parent", pi, "mci", mci, "self.alist", self.alist
+
+    def remove(self):
+        # remove things in the middle of the min heap
+        # takes in a value, finds the value in the array O(n)
+        # assign it to a min val
+        # bubble it down to remove it
+
+        
+
+minheap = MinHeap([4,7,5,9,10,6,8])
+print minheap.alist
+minheap.swap(1,4)
+print minheap.alist
+peak = minheap.peak()
+print peak
+print minheap.size()
+minheap.remove_peak()
+print minheap.alist
+
+
+
+
+
+
+
